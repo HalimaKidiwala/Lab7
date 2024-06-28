becef04edbcf87222c113fbc515b92bff1f0329d
halimakidiwala <hlimaaltafhussaini01@algomau.ca>
2024-06-28 16:13:53 -0400

    Initial commit: Added Lab7.php and related files

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+.footer {
+  background-color: #6c757d; 
+  padding: 20px 0; /* Adjust padding as needed */
+  margin-top: auto; /* Push footer to the bottom */
+  width: 100%; /* Full width of the viewport */
+}
+
+.footer p {
+  text-align: center; /* Center-align text within the footer */
+  margin: 0; /* Remove default margin */
+}
---
+++ php/behavior.php
@@ -0,0 +1,11 @@
+<?php
+    // Example PHP code for behavior.php
+    $message = "Make Something Interesting here....!";
+    echo "<p>$message</p>";
+?>
+
+
+
+
+
+
